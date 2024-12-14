1bc3298f4783ff97a3f273ca51affe86743f35ef
Atul Mane <maneatul225@gmail.com>
2024-12-14 15:57:33 +0530

    filtering

--- store/views.py
+++ store/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
+from store.filters import ProductFilter
 
 from .models import OrderItem, Product, Collection, Review
 from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializer
@@ -11,7 +14,19 @@ from .serializers import CollectionSerializer, ProductSerializer, ReviewSerializ
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
+    # Without library
+    # def get_queryset(self):
+    #     queryset = Product.objects.all()
+    #     collection_id = self.request.query_params.get("collection_id")
+    #
+    #     if collection_id is not None:
+    #         queryset = queryset.filter(collection_id=collection_id)
+    #
+    #     return queryset
+    #
     def get_serializer_context(self):
         return {"request": self.request}
 
